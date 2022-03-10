765266233cae43e4bed20d156f119ef33e33a6fc
Gabriel Ochsenhofer <gabs@gabs.tv>
2022-03-10 11:23:56 -0300

    test empty string

--- mask_test.go
+++ mask_test.go
@@ -8,6 +8,7 @@ import (
 
 func TestMaskStepped(t *testing.T) {
 	assert.Equal(t, "Lui* ** Sil**", maskStepped("Luis da Silva", 3, 3))
+	assert.Equal(t, "", maskStepped("", 3, 3))
 	assert.Equal(t, "Kev** *aco* **ger*", maskStepped("Kevin Bacon Rogers", 3, 3))
 	assert.Equal(t, "Le*** Je***ns", maskStepped("Leroy Jenkins", 2, 3))
 }
@@ -16,4 +17,6 @@ func TestMaskDocument(t *testing.T) {
 	assert.Equal(t, "***123545**", MaskDocument("32312354564"))
 	assert.Equal(t, "***.123.545-**", MaskDocument("323.123.5 4 5 - 64"))
 	assert.Equal(t, "***.463.432-**", MaskDocument("7A3.463.432-2Z"))
+	assert.Equal(t, "**", MaskDocument("12"))
+	assert.Equal(t, "", MaskDocument(""))
 }
